6de958a188d5602b21e8bf4aaf8622ce6f480519
SSujitX <ssujitxx@gmail.com>
2024-10-21 06:31:14 +0600

    setup 1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="facebook-page-scraper",
-    version="0.1.0",
+    version="0.0.1",
     description="Facebook page scraper is a python package that helps you scrape data from facebook page.",
     long_description=long_description,
     long_description_content_type="text/markdown",
